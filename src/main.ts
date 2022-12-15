9d5eaec19293d75af9d8bdb43e8e2aa9cc30f19e
Exzender <42044185+Exzender@users.noreply.github.com>
2022-12-15 12:27:16 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const app = createApp(App)
 const vuetify = createVuetify()
 
 if (Object.keys(window.Telegram.WebApp.themeParams).length === 0) {
-    window.location.href = import.meta.env.VITE_BOT_URL
+    window.location.href = 'https://ya.ru/'//import.meta.env.VITE_BOT_URL
 }
 
 app.use(vuetify)
